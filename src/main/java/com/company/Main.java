ab8328b971e8729729546096a9d98c054dbc46d3
Sergey <ya.sergey137@gmail.com>
2020-10-17 12:06:16 +0300

    serialization

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,6 +5,11 @@ import com.company.model.Store;
 public class Main {
 
     public static void main(String[] args) {
-        Store store = new Store();
+        Store store = Store.deserialize();
+        if (store == null) store = new Store();
+
+
+
+        store.serialize();
     }
 }
---
+++ src/main/java/com/company/model/Store.java
@@ -0,0 +1,54 @@
+package com.company.model;
+
+import com.company.model.parts.Part;
+import com.company.model.parts.PartType;
+import com.company.model.users.User;
+import com.company.model.users.UserType;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Store {
+    private Map<PartType, Part> parts;
+    private Map<UserType, User> users;
+
+    public Store() {
+        parts = new HashMap<>();
+        users = new HashMap<>();
+    }
+
+    public void add(PartType type, Part part) {
+        parts.put(type, part);
+    }
+    public static Store deserialize() {
+        Store store = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/store.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            store = (Store) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+        }
+        return store;
+    }
+    public void serialize() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("/tmp/store.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /tmp/employee.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
